60dbd9e959c6e58bff4f16ad97336c7dc79a2e4a
houssam072 <houssamsaleh072@gmail.com>
2023-08-18 01:04:48 -0700

    add new app [post]

---
+++ post/models.py
@@ -0,0 +1,21 @@
+from django.urls import reverse
+from django.utils import timezone
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='post-img/')
+
+    class Meta:
+        verbose_name = ("Post")
+        verbose_name_plural = ("Posts")
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('', kwargs={'pk':self.pk })
